bd0ac8c965a1efad91e7a6a67ef36a433672de41
Baishnawi Agrawal <145450500+Baish34@users.noreply.github.com>
2024-08-05 13:11:18 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import "./App.css";
+
+const MyComponent = () => {
+  return <h1>Hello, JSX!</h1>;
+};
+
+const name = "John";
+const Greeting = () => {
+  return <h1>Hello, {name}!</h1>;
+};
+
+const LinkTag = () => {
+  const url = "https://example.com";
+  return (
+    <a href={url} target="_blank">
+      Visit Example Website
+    </a>
+  );
+};
+
+const ImageTag = () => {
+  const imageUrl = "https://via.placeholder.com/150";
+  return (
+    <div>
+      <img src={imageUrl} alt="placeholder" />
+    </div>
+  );
+};
+
+const ProfileDetails = () => {
+  const details = {
+    name: "John Doe",
+    age: 25,
+    email: "john@gmail.com",
+    bio: "A passionate software developer.",
+  };
+
+  return (
+    <div>
+      <h2>Profile Details</h2>
+      <p>Name: {details.name}</p>
+      <p>Age: {details.age}</p>
+      <p>Email: {details.email}</p>
+      <p>Bio: {details.bio}</p>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <div>
+      {/* rendering JSX */}
+
+      <MyComponent />
+      <Greeting />
+      <LinkTag />
+      <ImageTag />
+      <ProfileDetails />
+    </div>
+  );
+}
